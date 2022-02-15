723b50b6e11776cbdf92e78f4e7b52c4ee7443cc
Laurena Fayad <laurenafayad@gmail.com>
2022-02-15 18:42:18 +0200

    Populating posts dynamically from database

--- feeds.js
+++ feeds.js
@@ -4,13 +4,11 @@ function getallFeeds(){
     var bodyFormData = new FormData();
     let token= window.localStorage.getItem('token');
     bodyFormData.append('token', token);
-    bodyFormData.append('function', 'GET');
-    // bodyFormData.append('post', 'POST');
-    
+    bodyFormData.append('function', 'GET');    
     
     axios({
         method: 'post',
-        url: 'http://localhost/fbmain/Facebook-Backend/post.php',
+        url: 'http://localhost/Facebook/Facebook-Backend/post.php',
         data: bodyFormData,
     })
     .then(function (response) {
--- homepage.js
+++ homepage.js
@@ -17,6 +17,37 @@ document.getElementById("friend-suggestions-page").addEventListener("click", fun
 
 document.getElementById("user_name").innerHTML = fname + " " + lname
 
+document.getElementById("load-more-btn").addEventListener("click", function(){
+    location.href = 'homepage.html'
+})
+
+//Populating Feed Posts
+var bodyFormData = new FormData();
+let token= window.localStorage.getItem('token');
+bodyFormData.append('token', token);
+bodyFormData.append('function', 'GET');    
+
+
+axios({
+    method: 'post',
+    url: 'http://localhost/Facebook/Facebook-Backend/post.php',
+    data: bodyFormData,
+})
+.then(function (response) {
+    for(var i in response.data){
+        let post_text = `${response.data[i].post_text}`;
+        let account_name = `${response.data[i].fname}` + " " + `${response.data[i].lname}`;
+        let post_date = `${response.data[i].post_date}`
+        let post_id = `${response.data[i].post_id}`
+        let account_id = `${response.data[i].id}`
+
+        let post_html = "<div class=post-container><div class=user-profile><img src=assets/profile-pic.png><div><p id=post_account_name>"+account_name+"</p><span id=post_date>"+post_date+"</span></div></div><p id=post-text>"+post_text+"</p><div class=post-likes><div class=activity-icons><div><a href=#><img src=assets/like-blue.png/></a><span>30</span></div></div></div></div>"
+
+        document.getElementById("posts").innerHTML += post_html
+    }           
+})
+
+//Adding post text to database on click
 document.getElementById("post-now-btn").addEventListener("click", function(){
     
     let data = [];
@@ -42,6 +73,5 @@ document.getElementById("post-now-btn").addEventListener("click", function(){
     })
     .catch(function (error) {
         console.log(error);
-    });
-    
+    });   
 })

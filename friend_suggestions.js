c9fd9f3aec1ab7db893ad4e0290b84d4e07ab064
Laurena Fayad <laurenafayad@gmail.com>
2022-02-15 14:27:40 +0200

    Minor fixes to JS file

--- friend_suggestions.js
+++ friend_suggestions.js
@@ -18,7 +18,25 @@ axios.get(friend_suggestions_api).then(response => {
     for (let i = 0; i<data.length; i++){
         let suggestion = `${data[i].fname}` + " " + `${data[i].lname}` 
         let suggestionID = `${data[i].id}`
-        let string = "<div class=suggestion><div class=left-suggestion><img src=assets/profile-pic.png><h4>"+suggestion+"</h4></div><div class=right-suggestion><a class=add-friend-btn href=#>Add Friend</a></div></div>"
-        document.getElementById("suggestions").innerHTML += string
+        let friend_suggestions_string = "<div class=suggestion><div class=left-suggestion><img src=assets/profile-pic.png><h4>"+suggestion+"</h4></div><div class=right-suggestion><a class=add-friend-btn id=" + suggestionID + " href=#>Add Friend</a></div></div>"
+        document.getElementById("suggestions").innerHTML += friend_suggestions_string 
     }
+
+    //Check if any add friend button is clicked and update DB accordingly
+    let add_friend_buttons =  document.getElementsByClassName("add-friend-btn")
+
+    for (let i = 0; i < add_friend_buttons.length; i++) {
+        
+        add_friend_buttons[i].addEventListener("click", function() {
+            requestID = this.getAttribute('id')
+            let add_friend_data = [];
+            let add_friend_api = "http://localhost/Facebook/Facebook-Backend/send_friend_request.php/?user1=" + current_user + "&user2=" + requestID
+            axios.get(add_friend_api).then(response => {
+                add_friend_data = response.data;
+                if(`${add_friend_data.status}` != null){
+                    document.getElementById(requestID).innerHTML = "Request Sent!"
+                }
+            })
+        });
+    } 
 })

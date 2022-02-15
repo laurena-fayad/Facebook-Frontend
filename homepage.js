e61a7087590bf46dab76a8250b5ff573df345168
Laurena Fayad <laurenafayad@gmail.com>
2022-02-15 17:17:06 +0200

    Linked post API to post button

--- homepage.js
+++ homepage.js
@@ -14,3 +14,34 @@ document.getElementById("friend_requests_btn").addEventListener("click", functio
 document.getElementById("friend-suggestions-page").addEventListener("click", function(){
     location.href = 'friend_suggestions.html'
 })
+
+document.getElementById("user_name").innerHTML = fname + " " + lname
+
+document.getElementById("post-now-btn").addEventListener("click", function(){
+    
+    let data = [];
+    let bodyFormData = new FormData();
+    let post= document.getElementById("text-area").value;
+    let token= window.localStorage.getItem('token');
+    bodyFormData.append('token', token);
+    bodyFormData.append('function', 'POST');
+    bodyFormData.append('post', post);
+    
+    axios({
+        method: 'post',
+        url: 'http://localhost/Facebook/Facebook-Backend/post.php',
+        data: bodyFormData,
+    })
+    .then(function (response) {
+        data = response.data
+        if(`${data.status}` == "You've just posted!") {
+            document.getElementById("post-status").innerHTML = "Post Successful!"
+        }else{
+            document.getElementById("post-status").innerHTML = "Error. Post Unsuccessful."    
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
+})
